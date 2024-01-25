3e521d54e5bbee7dbe44bf5457f9875eae373fb4
zzzang12 <wadevs12@gmail.com>
2024-01-25 15:44:48 +0900

    [Feat] echo 서버, health check 추가

---
+++ main.go
@@ -0,0 +1,22 @@
+package main
+
+import (
+	"github.com/labstack/echo/v4"
+	"github.com/labstack/echo/v4/middleware"
+	"net/http"
+)
+
+func main() {
+	e := echo.New()
+
+	e.Use(middleware.Logger())
+	e.Use(middleware.Recover())
+
+	e.GET("/api/health", healthCheck)
+
+	e.Logger.Fatal(e.Start(":1323"))
+}
+
+func healthCheck(c echo.Context) error {
+	return c.String(http.StatusOK, "Backend Alive")
+}
